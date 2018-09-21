2e4fba187d8b92ae7ca0ec644a92882c733ec796
Heshan Pathirana <101619999@student.swin.edu.au>
2018-09-21 10:05:06 +1000

    add new text font in the game

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -61,7 +61,7 @@ namespace CardGames
 			{
 				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.RoyalBlue, "GameFont", 0, 20);
 				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.RoyalBlue, "GameFont", 0, 30);
-				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.RoyalBlue, "GameFont", 0, 40);
+                SwinGame.DrawText("Player 2 score: " + myGame.Score(1), Color.RoyalBlue, "GameFont", 0, 40);
 				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 350, 50);
 			}
 			else
